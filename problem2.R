efe0724d5c7af4b1ba5c2f610317caa896b67d56
Hua Hua <hhua1@jhu.edu>
2012-11-13 13:14:24 -0500

    final

--- problem2.R
+++ problem2.R
@@ -1,6 +1,6 @@
-problem2<-read.table('mydata.csv',sep=',',header=T)
-attach(problem2)
+mydata<-read.table('mydata.csv',sep=',',header=T)
+attach(mydata)
 x<-tapply(Medal,INDEX=NOC,FUN=length)
-y<-tapply(Medal,INDEX=NOC,FUN=table)
+problem2<-tapply(Medal,INDEX=NOC,FUN=table)
 
-save(file='problem2.RData',list='x')
+save(file='problem2.RData',list='problem2')
